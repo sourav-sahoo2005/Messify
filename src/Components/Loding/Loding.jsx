66786bc9e440f57adb0b680417354f50e5f0841b
Sourav Sahoo <souravsahoo26320@gmail.com>
2026-02-04 22:09:09 +0530

    Loading component tested

---
+++ src/Components/Loding/Loding.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+
+
+const Loding = () => {
+  return (
+    <div className='h-screen bg-white text-6xl text-black'>
+        <h1>Loading...</h1>
+
+      
+    </div>
+  )
+}
+
+export default Loding
